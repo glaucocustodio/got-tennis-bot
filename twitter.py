61486090a886fa54eeae07057405e77419364e1e
Glauco Custódio <glauco.custodio@gmail.com>
2023-06-14 19:05:52 +0100

    use library to tweet

--- twitter.py
+++ twitter.py
@@ -1,32 +1,20 @@
-import requests
-from requests_oauthlib import OAuth1
-import json, os
+import pytwitter
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def tweet(message):
-    url = "https://api.twitter.com/2/tweets"
+  consumer_key = os.environ['TWITTER_CONSUMER_KEY']
+  consumer_secret = os.environ['TWITTER_CONSUMER_SECRET']
+  access_token = os.environ['TWITTER_ACCESS_TOKEN']
+  access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
 
-    payload = json.dumps({"text": message})
+  twitter_api = pytwitter.Api(
+    consumer_key=consumer_key,
+    consumer_secret=consumer_secret,
+    access_token=access_token,
+    access_secret=access_token_secret
+  )
 
-    consumer_key = os.environ['TWITTER_CONSUMER_KEY']
-    consumer_secret = os.environ['TWITTER_CONSUMER_SECRET']
-    access_token = os.environ['TWITTER_ACCESS_TOKEN']
-    access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
-
-    oauth = OAuth1(
-        consumer_key,
-        consumer_secret,
-        access_token,
-        access_token_secret
-    )
-
-    headers = {
-        'Content-Type': 'application/json',
-    }
-
-    response = requests.post(url, headers=headers, data=payload, auth=oauth)
-    response.raise_for_status()
-
-    return response
+  return twitter_api.create_tweet(text=message)
